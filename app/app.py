ea574c6338fd659a2629d5730329b4d80b2a62db
tatigabru <travellingtati@gmail.com>
2020-12-04 17:39:35 +0000

    Fix typing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 from PIL import Image
-
+from typing import List
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
