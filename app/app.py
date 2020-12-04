6cb07c294b2aba6987f8c1f950de2df75128a84d
tatigabru <travellingtati@gmail.com>
2020-12-04 17:33:57 +0000

    Update app2

--- app/app.py
+++ app/app.py
@@ -4,22 +4,27 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 from PIL import Image
-#from retinaface.pre_trained_models import get_model
-#from retinaface.utils import vis_annotations
+
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
 
-#@st.cache
-#def cached_model():
-#    m = get_model("resnet50_2020-07-20", max_size=1048, device="cpu")
-#    m.eval()
-#    return m
-#model = cached_model()
+@st.cache
+def cached_model(x_valid: List):
+    """
+    Load logreg model from disk 
+    """
+    model = pickle.load(open('model.txt', 'rb'))
+    y_pred = model.predict(x_valid)
+    # result = model.score(x_valid, y_valid)
+    # print(result)
+    return y_pred
 
-st.title("CTG signals")
+#result = cached_model(x_valid)
 
-uploaded_file = st.file_uploader("Choose an image...", type="png")
+st.title("Cardiotocography signals")
+
+uploaded_file = st.file_uploader("Load patient's cardiotocography (CTG)...", type="png")
 if uploaded_file is not None:
     image = np.array(Image.open(uploaded_file))
     st.image(image, caption="CTG", use_column_width=True)
@@ -27,7 +32,6 @@ if uploaded_file is not None:
 st.write("")
 st.write("EHR")
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-st.line_chart(chart_data)
+st.write("EGA week: 39")
+st.write("Parity: 1")
+st.write("Age: 30")
