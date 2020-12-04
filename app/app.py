b6ae0c180825624deaab08e6b941da62ffdfe6cb
tatigabru <travellingtati@gmail.com>
2020-12-04 15:22:34 +0000

    Update gitignore

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
+from PIL import Image
 #from retinaface.pre_trained_models import get_model
 #from retinaface.utils import vis_annotations
 
@@ -14,19 +15,19 @@ st.set_option("deprecation.showfileUploaderEncoding", False)
 #    m = get_model("resnet50_2020-07-20", max_size=1048, device="cpu")
 #    m.eval()
 #    return m
-
-
-model = cached_model()
+#model = cached_model()
 
 st.title("CTG signals")
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
+uploaded_file = st.file_uploader("Choose an image...", type="png")
 if uploaded_file is not None:
     image = np.array(Image.open(uploaded_file))
-    st.image(image, caption="Before", use_column_width=True)
-    st.write("")
-    st.write("Detecting faces...")
+    st.image(image, caption="CTG", use_column_width=True)
 
+st.write("")
+st.write("EHR")
 
-    st.image(visualized_image, caption="After", use_column_width=True)
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
